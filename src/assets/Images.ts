5210786ebbb4cf793fc0e5fb7c7e3f885e280760
longnx.bhsoft <longnx.bhsoft@gmail.com>
2021-04-10 14:19:11 +0700

    feat(assets): assets - add new assests

---
+++ src/assets/Images.ts
@@ -0,0 +1,10 @@
+export const images = {};
+
+export const icons = {
+  login: {
+    email: require('./icons/icon-mail.png'),
+    pass: require('./icons/icon-padlock.png'),
+    fullName: require('./icons/icon-account.png'),
+    exit: require('./icons/icon-close.png'),
+  },
+};
