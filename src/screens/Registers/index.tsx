4b6e8205edc9ebbb5c70bcbe8909128dd3a10c5c
longnx.bhsoft <longnx.bhsoft@gmail.com>
2021-04-10 16:40:32 +0700

    feat(screens): register - add new register screens

--- src/screens/Registers/index.tsx
+++ src/screens/Registers/index.tsx
@@ -1,18 +1,43 @@
 import React from 'react';
-import {StyleSheet} from 'react-native';
-import {View} from 'react-native-ui-lib';
-import {Colors} from '../../assets';
+import {StyleSheet, TouchableOpacity} from 'react-native';
+import {View, Text} from 'react-native-ui-lib';
+import {Colors, Icons, Metrics} from '../../assets';
 import {Container} from '../components';
+import {Inputs} from '../components';
 
-const LoginScreen = () => {
+const RegisterScreen = (props: {navigation: {goBack: () => void}}) => {
+  const goToLogin = () => {
+    props.navigation.goBack();
+  };
   return (
     <Container
       backgroundColor={Colors.white}
       backgroundBody={Colors.white}
       barStyle="dark-content">
-      <View flex-10>
-        <View flex-8 style={styles.container} />
+      <View flex-8 style={styles.container} centerH>
         <View flex-2 />
+        <View height={60} center>
+          <Text style={styles.fonts}>Đăng kí</Text>
+        </View>
+        <View flex-2 />
+        <View height={150} centerH backgroundColor={Colors.black01}>
+          <Inputs
+            leftIcons={Icons.login.email}
+            placeholder={'Số điện thoại'}
+            keyboardType={'phone-pad'}
+          />
+          <Inputs leftIcons={Icons.login.pass} placeholder={'Mật khẩu'} />
+          <Inputs leftIcons={Icons.login.pass} placeholder={'Mật khẩu'} />
+        </View>
+        <View flex-2 />
+        <TouchableOpacity style={styles.Button}>
+          <Text style={styles.titleButton}>Đăng kí</Text>
+        </TouchableOpacity>
+      </View>
+      <View flex-2 center>
+        <TouchableOpacity onPress={goToLogin}>
+          <Text color={Colors.blueNavy}>Đăng nhập?</Text>
+        </TouchableOpacity>
       </View>
     </Container>
   );
@@ -32,6 +57,23 @@ const styles = StyleSheet.create({
     elevation: 5,
     backgroundColor: Colors.white,
   },
+  fonts: {
+    fontSize: 24,
+    color: Colors.orangeCarrot,
+  },
+  Button: {
+    width: Metrics.screen.width - 60,
+    height: 60,
+    backgroundColor: Colors.orangeCarrot,
+    borderRadius: 30,
+    justifyContent: 'center',
+    marginBottom: 30,
+  },
+  titleButton: {
+    alignSelf: 'center',
+    fontSize: 20,
+    color: Colors.white,
+  },
 });
 
-export default LoginScreen;
+export default RegisterScreen;
